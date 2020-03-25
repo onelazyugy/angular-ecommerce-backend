1ab3f2cea70eb70f58fa7d1605f804c7f099ea26
Viet <viet@Viets-MBP.attlocal.net>
2020-03-24 22:16:02 -0400

    added price to Item

--- src/main/java/com/vietle/angularecommercebackend/domain/Item.java
+++ src/main/java/com/vietle/angularecommercebackend/domain/Item.java
@@ -2,10 +2,13 @@ package com.vietle.angularecommercebackend.domain;
 
 import lombok.*;
 
+import java.math.BigDecimal;
+
 @Builder
 @Data
 public class Item {
     private int id;
     private String name;
     private String imgurl;
+    private BigDecimal price;
 }
